29736735f6be97c409da75ed737a99d287c901f5
jdwelu <58574621+jdwelu@users.noreply.github.com>
2022-02-22 13:22:57 -0600

    Update cleaner1.bat

--- cleaner1.bat
+++ cleaner1.bat
@@ -8,21 +8,27 @@ REM First you change the Drive Letter to the one where the folder is located
 REM Example: [drive letter]: do not include the brackets (do not forget colon)
 REM Second you change the path in between the parentheses
 REM Example: cd "[your path]" do not include the brackets
-
+REM Lastly, change the path in the if exists line to match your path EXACTLY!!! 
 
 echo starting program, cleaner
-C: 
-cd "C:\Users\Administrator\Desktop\TEST_FOLDER"
 timeout 3
+cls
+
+C:
+if exist "C:\Users\Administrator\Desktop\TEST_FOLDER" (echo folder exists) else exit
+timeout 3
+cd "C:\Users\Administrator\Desktop\TEST_FOLDER"
 cls
 
 
 echo deleting all files
+timeout 3
 del *.* /s /f /q
 cls
 
 
 echo deleting all folders and subdirectories
+timeout 3
 rmdir . /S /Q 2>nul
 cls
 
